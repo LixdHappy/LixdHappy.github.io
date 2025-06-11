aad8f9af015f153f586bf8ab06ca874a65989944
LixdHappy <gb-1@foxmail.com>
2025-06-11 21:47:28 +0800

    Site updated: 2025-06-11 21:47:27

---
+++ js/copy share.js
@@ -0,0 +1,26 @@
+// 防抖全局计时器
+let TT = null;    //time用来控制事件的触发
+// 防抖函数:fn->逻辑 time->防抖时间
+function debounce(fn, time) {
+    if (TT !== null) clearTimeout(TT);
+    TT = setTimeout(fn, time);
+}
+
+// 复制提醒
+document.addEventListener("copy", function () {
+    debounce(function () {
+        new Vue({
+            data: function () {
+                this.$notify({
+                    title: "哎嘿！复制成功🍬",
+                    message: "希望复制思考的同时记得前人的努力噢~",
+                    position: 'top-left',
+                    offset: 50,
+                    showClose: true,
+                    type: "success",
+                    duration: 5000
+                });
+            }
+        })
+    }, 300);
+})
