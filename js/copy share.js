335c0fe38b2a7e4a93e3f2afa46df4e21ad8cdff
LixdHappy <gb-1@foxmail.com>
2025-06-14 17:48:48 +0800

    Site updated: 2025-06-14 17:48:47

--- js/copy share.js
+++ js/copy share.js
@@ -24,3 +24,24 @@ document.addEventListener("copy", function () {
         })
     }, 300);
 })
+
+// f12提醒但不禁用
+document.onkeydown = function (e) {
+  if (123 == e.keyCode || (e.ctrlKey && e.shiftKey && (74 === e.keyCode || 73 === e.keyCode || 67 === e.keyCode)) || (e.ctrlKey && 85 === e.keyCode)) {
+    debounce(function () {
+      new Vue({
+        data: function () {
+          this.$notify({
+            title: "你已被发现😜",
+            message: "？？？？你在做什么！？",
+            position: 'top-left',
+            offset: 50,
+            showClose: true,
+            type: "warning",
+            duration: 5000
+          });
+        }
+      })
+    }, 300);
+  }
+};
